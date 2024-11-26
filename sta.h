4518d33d003c2df26286f6c469d8d89683923877
Valdinei Rodrigues <valdinei.conceicao@itec.ufpa.br>
2024-11-26 15:33:33 -0300

    Change var type

--- sta.h
+++ sta.h
@@ -4,6 +4,6 @@ std::string getPerBeamRSS(int radio_sock);
 
 void resetCam();
 
-std::vector<uchar> getRGB(rs2::pipeline p, rs2::align align_to_color);
+std::vector<unsigned char> getRGB(rs2::pipeline p, rs2::align align_to_color);
 
-std::vector<uchar> getDepth(rs2::pipeline p, rs2::align align_to_color);
+std::vector<unsigned char> getDepth(rs2::pipeline p, rs2::align align_to_color);
